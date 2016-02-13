d6ca26a43a2629981c8a5ed8d205b9b95194160e
ralph <noiseartist@gmail.com>
2016-02-13 02:16:30 -0800

    added parallax scrolling and skrollr effects

--- js/main.js
+++ js/main.js
@@ -1,52 +1,64 @@
 $('document').ready(function(){
 
-		$('#me-trig')
-		.mouseenter(function(){
-			$('#me-trig').css({"font-size": "2.1rem", "color": "#00BCD4"});
-			$('#me').fadeIn(400);
-			})
-		.mouseleave(function(){
-				$('#me-trig').css({"font-size": "1.8rem", "color": "black"});
-				$('#me').hide();
-			});
-
-		$('#code-trig')
-		.mouseenter(function(){
-			$('#code-trig').css({"font-size": "2.1rem", "color": "#00BCD4"});
-			$('#code-boy').fadeIn(400);
-		})
-		.mouseleave(function(){
-			$('#code-trig').css({"font-size": "1.8rem", "color": "black"});
-			$('#code-boy').hide();
-		});
-
-		$('#drum-trig')
-		.mouseenter(function(){
-			$('#drum-trig').css({"font-size": "2.1rem", "color": "#00BCD4"});
-			$('#drum').fadeIn(400);
-		})
-		.mouseleave(function(){
-				$('#drum-trig').css({"font-size": "1.8rem", "color": "black"});
-				$('#drum').hide();
-		});
-
-		$('#dot-trig')
-		.mouseenter(function(){
-			$('#dot-trig').css({"font-size": "2.1rem", "color": "#00BCD4"});
-			$('#dots').fadeIn(400);
-		})
-		.mouseleave(function(){
-			$('#dot-trig').css({"font-size": "1.8rem", "color": "black"});
-			$('#dots').hide();
-		});
-
-		$('#app-trig').mouseenter(function(){
-			$('#app-trig').css({"font-size": "2.1rem", "color": "color: ##00BCD4"});
-			$('#react').fadeIn(400);
-		})
-		.mouseleave(function(){
-			$('#app-trig').css({"font-size": "1.8rem", "color": "black"});
-			$('#react').hide();
+	var s = skrollr.init({
+		forceHeight: false,
+		// constants: {
+		// 	box: 150p;
+		// }
 	});
+	$('parallax-window').parallax({
+		naturalWidth: 1200,
+		naturalHeight: 1600
+	});
+
+	$(window).trigger('resize').trigger('scroll');
+	// 	$('#me-trig')
+	// 	.mouseenter(function(){
+	// 		$('#me-trig').css({"font-size": "2.8rem", "color": "#00BCD4"});
+	// 		$('#me').toggle("slide", 400);
+	// 		})
+	// 	.mouseleave(function(){
+	// 			$('#me-trig').css({"font-size": "2.5rem", "color": "black"});
+	// 			$('#me').hide();
+	// 		});
+	//
+	// 	$('#code-trig')
+	// 	.mouseenter(function(){
+	// 		$('#code-trig').css({"font-size": "2.8rem", "color": "#00BCD4"});
+	// 		$('#code-keys').toggle("blind", 400);
+	// 	})
+	// 	.mouseleave(function(){
+	// 		$('#code-trig').css({"font-size": "2.5rem", "color": "black"});
+	// 		$('#code-keys').hide();
+	// 	});
+	//
+	// 	$('#drum-trig')
+	// 	.mouseenter(function(){
+	// 		$('#drum-trig').css({"font-size": "2.8rem", "color": "#00BCD4"});
+	// 		$('#drum').toggle("clip", 400);
+	// 	})
+	// 	.mouseleave(function(){
+	// 			$('#drum-trig').css({"font-size": "2.5rem", "color": "black"});
+	// 			$('#drum').hide();
+	// 	});
+	//
+	// 	$('#dot-trig')
+	// 	.mouseenter(function(){
+	// 		$('#dot-trig').css({"font-size": "2.8rem", "color": "#00BCD4"});
+	// 		$('#dots').toggle("clip", 400);
+	// 	})
+	// 	.mouseleave(function(){
+	// 		$('#dot-trig').css({"font-size": "2.5rem", "color": "black"});
+	// 		$('#dots').hide();
+	// 	});
+	//
+	// 	$('#app-trig').mouseenter(function(){
+	// 		$('#app-trig').css({"font-size": "2.8rem", "color": "color: ##00BCD4"});
+	// 		$('#react').toggle("slide", 400);
+	// 	})
+	// 	.mouseleave(function(){
+	// 		$('#app-trig').css({"font-size": "2.5rem", "color": "black"});
+	// 		$('#react').hide();
+	// });
 
 });
